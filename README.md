0777ffc7b3aa0fda6d2953c46d8530a92a7cd99f
Vicente Farias <armando3443op@gmail.com>
2023-06-16 17:41:38 -0400

    relaciones creadas

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ingenieria-de-Software
+# Con Django # 
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+
+# En PostgreSQL #
+
+INSERT INTO auth_group VALUES(0, 'Default');
+INSERT INTO auth_group VALUES(1, 'Administrador');
+INSERT INTO registration_profile VALUES(0, 'No', 'No', 1, 1);
--- estudio/models.py
+++ estudio/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import Group, User #importa los modelos Group y user
 from django.db import models #importa los metodos necesarios para trabajar con modellos
 
+STATE_CHOICES= [
+    ("O", "Odontologo"),
+    ("S", "Secretaria"),
+]
+
 class Paciente(models.Model):
     rut = models.CharField(max_length=100, null=True, blank=True, verbose_name='Rut Paciente')
     nombre = models.CharField(max_length=100, null=True, blank=True, verbose_name='Nombre Paciente')
@@ -19,14 +24,7 @@ class Paciente(models.Model):
     def __str__(self):
         return self.nombre
     
-class Cita(models.Model):
-    fechaAtencion = models.DateTimeField(auto_now_add=True,verbose_name='Fecha Atencion')
-    horaInicio = models.DateTimeField(auto_now_add=True,verbose_name='Hora Inicio')
-    horaFinal = models.DateTimeField(auto_now_add=True,verbose_name='Hora Final')
-    procedimiento = models.CharField(max_length=100, null=True, blank=True, verbose_name='Procedimiento Paciente')
-    diagnostico = models.CharField(max_length=100, null=True, blank=True, verbose_name='Diagnostico Paciente')
-    extra = models.CharField(max_length=100, null=True, blank=True, verbose_name='Extra Paciente')
-    estado = models.CharField(max_length=100, null=True, blank=True, default='Activo', verbose_name='Estado')
+
     
 class Trabajador(models.Model):
     rut = models.CharField(max_length=100, null=True, blank=True, verbose_name='Rut Trabajador')
@@ -36,7 +34,7 @@ class Trabajador(models.Model):
     correo = models.CharField(max_length=100, null=True, blank=True, verbose_name='Correo Trabajador')
     edad = models.CharField(max_length=100, null=True, blank=True, verbose_name='Edad Trabajador')
     direccion = models.CharField(max_length=100, null=True, blank=True, verbose_name='Direccion Trabajador')
-    estado = models.CharField(max_length=100, null=True, blank=True, default='Activo', verbose_name='Estado')   
+    state = models.CharField(max_length=1,choices=STATE_CHOICES, null=False, blank=False, default="O")  
     created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha Creación')
     updated = models.DateTimeField(auto_now=True,verbose_name='Fecha Actualización')
     class Meta:
@@ -45,6 +43,17 @@ class Trabajador(models.Model):
         ordering = ['nombre']   
     def __str__(self):
         return self.nombre
+
+class Cita(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    trabajador = models.ForeignKey(Trabajador, on_delete=models.CASCADE)
+    fechaAtencion = models.DateTimeField(auto_now_add=True,verbose_name='Fecha Atencion')
+    horaInicio = models.DateTimeField(auto_now_add=True,verbose_name='Hora Inicio')
+    horaFinal = models.DateTimeField(auto_now_add=True,verbose_name='Hora Final')
+    procedimiento = models.CharField(max_length=100, null=True, blank=True, verbose_name='Procedimiento Paciente')
+    diagnostico = models.CharField(max_length=100, null=True, blank=True, verbose_name='Diagnostico Paciente')
+    extra = models.CharField(max_length=100, null=True, blank=True, verbose_name='Extra Paciente')
+    estado = models.CharField(max_length=100, null=True, blank=True, default='Activo', verbose_name='Estado')
 
 class Cargo(models.Model):
     nombre = models.CharField(max_length=100, null=True, blank=True, verbose_name='Nombre Cargo')
